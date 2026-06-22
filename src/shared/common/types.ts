278ce35c93d17be93c2d7b99cd3d4050914244da
nadia <115858781+nadia8666@users.noreply.github.com>
2026-06-22 08:21:07 -0700

    remove nominal

--- src/shared/common/types.ts
+++ src/shared/common/types.ts
@@ -33,5 +33,5 @@ export type RS = ReplicatedStorage & {
 type StandardRobloxTypes = Folder | Model | Part | Attachment | ReplicatedStorage;
 
 export type Exclusive<T> = {
-	[K in keyof T]: K extends keyof StandardRobloxTypes ? never : K extends string ? K : never;
+	[K in keyof T]: K extends keyof StandardRobloxTypes ? never : K extends `_nominal_${string}` ? never : K extends string ? K : never;
 }[keyof T];
